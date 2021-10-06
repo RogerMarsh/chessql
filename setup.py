d72b652d6c54e9c2920db111b32d143249d53d0b
Roger Marsh <roger.marsh@solentware.co.uk>
2021-10-06 08:18:50 +0000

    Add pyproject.toml and setup.cfg for 'python -m pip ...' commands.
    setup.py retained for 'python setup.py ...' commands until this is no
    longer supported, but modified to rely on setup.cfg.
    
    Bump version number with .dev0 suffix to test providing a URL, or path
    to a html file, containing links to archives for the --find-links
    argument to 'python -m pip ...' commands.

--- setup.py
+++ setup.py
@@ -1,37 +1,16 @@
 # setup.py
 # Copyright 2017 Roger Marsh
 # Licence: See LICENCE (BSD licence)
-"""chessql setup file."""
+"""chessql setup file.
+
+Retained so 'python setup.py ...' can be used by my build scripts until
+conversion to 'python -m pip ...'.
+
+All setup() arguments are set in setup.cfg file.
+"""
 
 from setuptools import setup
 
 if __name__ == "__main__":
 
-    long_description = open("README").read()
-
-    setup(
-        name="chessql",
-        version="2.0.2",
-        description="Chess Query Language (cql) parser",
-        author="Roger Marsh",
-        author_email="roger.marsh@solentware.co.uk",
-        url="http://www.solentware.co.uk",
-        packages=[
-            "chessql",
-            "chessql.core",
-        ],
-        long_description=long_description,
-        license="BSD",
-        classifiers=[
-            "License :: OSI Approved :: BSD License",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Operating System :: OS Independent",
-            "Topic :: Software Development",
-            "Topic :: Games/Entertainment :: Board Games",
-            "Intended Audience :: Developers",
-            "Development Status :: 3 - Alpha",
-        ],
-    )
+    setup()
