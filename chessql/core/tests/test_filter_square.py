add1c732ec94e6ec4b7777e0067246d1121d6476
Roger Marsh <roger.marsh@solentware.co.uk>
2025-04-07 07:55:48 +0000

    Add 'square' filter unittests to check status of created variable.

--- chessql/core/tests/test_filter_square.py
+++ chessql/core/tests/test_filter_square.py
@@ -31,11 +31,30 @@ class FilterSquare(verify.Verify):
     def test_127_square_04_variable_in_02_filter(self):
         self.verify("square x in R", [], returncode=1)
 
-    def test_127_square_04_variable_in_03_filter_body(self):  # chessql wrong.
-        self.verify(
+    def test_127_square_04_variable_in_03_filter_body(self):
+        con = self.verify(
             "square x in R btm",
             [(3, "Square"), (4, "PieceDesignator"), (4, "BTM")],
         )
+        square = con.children[-1].children[-1]
+        self.assertEqual(square.name, "x")
+        self.assertEqual(square.name in con.definitions, True)
+
+    def test_127_square_04_variable_in_04_variable_type_ok(self):
+        self.verify(
+            "x=q square x in R btm",
+            [
+                (3, "Assign"),
+                (4, "Variable"),
+                (4, "PieceDesignator"),
+                (3, "Square"),
+                (4, "PieceDesignator"),
+                (4, "BTM"),
+            ],
+        )
+
+    def test_127_square_04_variable_in_05_variable_type_wrong(self):
+        self.verify("x=1 square x in R btm", [], returncode=1)
 
     def test_127_square_05_all(self):
         self.verify("square all", [], returncode=1)
@@ -49,11 +68,30 @@ class FilterSquare(verify.Verify):
     def test_127_square_07_all_variable_in_02_filter(self):
         self.verify("square all x in R", [], returncode=1)
 
-    def test_127_square_07_all_variable_in_03_filter_body(self):  # wrong.
-        self.verify(
+    def test_127_square_07_all_variable_in_03_filter_body(self):
+        con = self.verify(
             "square all x in R btm",
             [(3, "SquareAll"), (4, "PieceDesignator"), (4, "BTM")],
         )
+        square = con.children[-1].children[-1]
+        self.assertEqual(square.name, "x")
+        self.assertEqual(square.name in con.definitions, True)
+
+    def test_127_square_07_all_variable_in_04_variable_type_ok(self):
+        self.verify(
+            "x=q square all x in R btm",
+            [
+                (3, "Assign"),
+                (4, "Variable"),
+                (4, "PieceDesignator"),
+                (3, "SquareAll"),
+                (4, "PieceDesignator"),
+                (4, "BTM"),
+            ],
+        )
+
+    def test_127_square_07_all_variable_in_04_variable_type_wrong(self):
+        self.verify("x=1 square all x in R btm", [], returncode=1)
 
 
 if __name__ == "__main__":
